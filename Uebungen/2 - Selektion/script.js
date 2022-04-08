d6e11ea9b14f68ca9e5c74ad719447dbb8f3b1d2
m-mattia-m <59088904+m-mattia-m@users.noreply.github.com>
2022-04-08 14:58:59 +0200

    Changes to the introductory course

--- Uebungen/2 - Selektion/script.js
+++ Uebungen/2 - Selektion/script.js
@@ -1,61 +1,43 @@
 console.log('Start');
 
 /*   Aufgabe 1: Selektion 1.0
-*   -----------------------------------
-*   Wenn Zahl grösser als 0 gebe aus: die Zahl  ist postiv
-*   Falls Zahl Kleiner als 0 ist gebe aus: die Zahl ist negativ
-*/
+ *   -----------------------------------
+ *   Wenn Zahl grösser als 0 gebe aus: die Zahl  ist postiv
+ *   Falls Zahl Kleiner als 0 ist gebe aus: die Zahl ist negativ
+ */
 
 /*   Aufgabe 2: Selektion 2.0
-*   -----------------------------------
-*   Wenn Zahl grösser als 0 und kleiner als 10: die Zahl liegt dazwischen
-*   Ansonsten gebe aus: die Zahl liegt nicht im gültigen Rahmen
-*/
+ *   -----------------------------------
+ *   Wenn Zahl grösser als 0 und kleiner als 10: die Zahl liegt dazwischen
+ *   Ansonsten gebe aus: die Zahl liegt nicht im gültigen Rahmen
+ */
 
-/*   Aufgabe 3: Boolsches Algebra 1.0
-*   -----------------------------------
-*   Prüfe ob beide Eingaben korrekt sind:
-*   Falls beide die Bediengung erfüllen soll true ausgegeben werden.
-*   Falls eine der beiden Bediengungen erfüllt ist soll trotzdem false ausgegeben werden. 
-*/
+/*   Aufgabe 3: Noten Function
+ *   -----------------------------------
+ *   Bei Noten unter 4: ungenügende Note
+ *   Bei Noten über 4: genügende Note
+ */
 
-/*   Aufgabe 4: Boolsches Algebra 2.0
-*   -----------------------------------
-*   Prüfe ob eine der beiden Eingaben korrekt ist:
-*   Falls eine Bediengung erfüllt ist soll true ausgegeben werden.
-*   Wenn keine Bediengung erfüllt ist soll false ausgegeben werden. 
-*/
-
-/*   Aufgabe 5: Noten Function
-*   -----------------------------------
-*   Bei Noten unter 4: ungenügende Note
-*   Bei Noten über 4: genügende Note
-*/
-
-/*   Aufgabe 6: Switch-Case
-*   -----------------------------------
-*   Erstelle eine Klasse Person
-*/
-
-/*   Aufgabe 7: Trinkspiel
-*   -----------------------------------
-*   Erstelle eine Function welche je nach Alter folgendes zurück gibt:
-*   bei Alter unter 18 : nichts da!
-*   zwischen 18 und 45: zwei Birchen gehen
-*   zwischen 50 und 80: ein Whiskey
-*   An den Rest: Her mit dem Whiskey!
-*/
+/*   Aufgabe 4: Trinkspiel
+ *   -----------------------------------
+ *   Erstelle eine Function welche je nach Alter folgendes zurück gibt:
+ *   bei Alter unter 18 : nichts da!
+ *   zwischen 18 und 45: zwei Birchen gehen
+ *   zwischen 50 und 80: ein Whiskey
+ *   An den Rest: Her mit dem Whiskey!
+ */
 
 /*
-*   Aufgabe 8: BMI
-*   -----------------------------------
-*   Schreibe eine Funktion, die den "body mass index" berechnet. (bmi = weight / height ^ 2).
-*
-*   if bmi <= 18.5 return "Underweight"
-*
-*   if bmi <= 25.0 return "Normal"
-*
-*   if bmi <= 30.0 return "Overweight"
-*
-*   if bmi > 30 return "Obese"
-*/
+ *   Aufgabe 5: BMI
+ *   -----------------------------------
+ *   Schreibe eine Funktion, die den "body mass index" berechnet. (bmi = weight / height ^ 2).
+ *   if bmi <= 18.5 return "Underweight"
+ *   if bmi <= 25.0 return "Normal"
+ *   if bmi <= 30.0 return "Overweight"
+ *   if bmi > 30 return "Obese"
+ */
+
+/*   Aufgabe 6: Switch-Case (Freiwillig wenn genügend Zeit)
+ *   -----------------------------------
+ *   Erstelle eine Klasse Person
+ */
--- Uebungen/3 - Iteration/script.js
+++ Uebungen/3 - Iteration/script.js
@@ -1,81 +1,47 @@
 console.log('Start');
 
 /*   Aufgabe 1:  While-Schleife
-*   -----------------------------------
-*   While-Schleife erstellen, welche Werte von 1-10 und ausgeben
-*/
+ *   -----------------------------------
+ *   While-Schleife erstellen, welche Werte von 1-10 und ausgeben
+ */
 
 /*   Aufgabe 2: Do...While-Schleife
-*   -----------------------------------
-*   Do-While Schleife erstellen, welche Werte von 1-10 und ausgeben
-*/
+ *   -----------------------------------
+ *   Do-While Schleife erstellen, welche Werte von 1-10 und ausgeben
+ */
 
 /*
-*   Aufgabe 3: For-Schliefe
-*   -----------------------------------
-*   erstelle eine For-Schleife, welche das folgende Ausgibt
-*
-*   3
-*   9
-*   27
-*   81
-*   243
-*   729
-*/
+ *   Aufgabe 3: For-Schliefe
+ *   -----------------------------------
+ *   erstelle eine For-Schleife, welche das folgende Ausgibt
+ *
+ *   3
+ *   9
+ *   27
+ *   81
+ *   243
+ *   729
+ */
 
-/*   Aufgabe 4: Multiplikation 
-*   -----------------------------------
-*   Eine Multiplikation Function erstellen
-*
-*   Die Function soll durch Addition und Subtraktion eine Multiplikation nachbauen
-*
-*   Bsp 1: 5 * 3 = 5 + 5 + 5 = 15
-*   Bsp 2: -3 * 4 = -3 + -3 + -3 + -3 = -12
-*
-*/
+/*   Aufgabe 4: Durchschnittsberechnung
+ *   -----------------------------------
+ *   Man erhält ein Array aus Zahlen und soll den durchschnitt berechnen
+ *
+ *
+ *   Vorgehen:
+ *       1. For Schleife erstellen
+ *       2. Zahl zum durchschnitt hinzufügen und Schleifenvariable um 1 erhöhen
+ *       3. durchschnitt berechnen
+ */
 
-
-/*   Aufgabe 5: Durchschnittsberechnung
-*   -----------------------------------
-*   Man erhält ein Array aus Zahlen und soll den durchschnitt berechnen
-*
-*
-*   Vorgehen:
-*       1. For Schleife erstellen
-*       2. Zahl zum durchschnitt hinzufügen und Schleifenvariable um 1 erhöhen
-*       3. durchschnitt berechnen
-*/
-
-/*   Aufgabe 6: Median
-*   -----------------------------------
-*   Du erhälst ein Array mit Zahlen, gebe den Median aus
-*   Median ist der Wert der in der Mitte eines Datensatzes (Arrays) liegt 
-*
-*   Bsp. 1: [1 ,3, 6, 6, 8, 9, 12,15, 16] => Median: 8
-*   Bsp. 2: [-4, 3, 3, 4, 5, 9] => Median: 3.5 
-*   (Weil es eine gerade Anzahl an Elementen im Array gibt muss der Durchschnitt der beiden Datensätze genommen werden, die am nächsten an der Mitte liegen)
-*
-*   Vorgehen:
-*       1. Array der Grösse nach sortieren
-*       2. Mitte des Arrays finden 
-*       (Tipp: (array.length % 2) zeigt an ob es eine gerade Anzahl Elemente hat oder nicht)
-*       2.1 Falls es eine ungerade Anzahl Elemente hat:
-*           Die Mitte holen und ausgeben 
-*           (Tipp: Mitte = (Anzahl Elemente + 1) / 2)
-*       2.2 Falls es eine gerade Anzahl Elemente hat:
-*           Die Mitte berechnen und diese Zahl umwandeln in int
-*           (Tipp: Mitte = (Anzahl Elemente + 1) / 2) & (Anzahl Elemente / 2
-*           Man erhält zwei Zahlen, diese Elemente holt man aus dem Array und berechnet den Durchschnitt)
-*/
-
-/*   Aufgabe 7: Summe aller positiven Zahlen
-*   -----------------------------------
-*   Du erhälst ein Zahlen Array und gibst die Summe aller positiven Zahlen zurück
-*
-*   Bsp: [1,-4,7,12] => 1 + 7 + 12 = 20
-*
-*   Vorgehen:
-*       1. For Schleife erstellen
-*       2. Ist die Zahl grösser als 0?
-*           2.1 Zahl zum Resultat hinzufügen
-*/
+/*   Aufgabe 5: Summe aller positiven Zahlen
+ *   -----------------------------------
+ *   Du erhälst ein Zahlen Array und gibst die Summe aller positiven Zahlen zurück
+ *
+ *   Bsp: [1,-4,7,12] => 1 + 7 + 12 = 20
+ *
+ *   Vorgehen:
+ *       1. For Schleife erstellen
+ *       2. Ist die Zahl grösser als 0?
+ *           2.1 Zahl zum Resultat hinzufügen
+ */
